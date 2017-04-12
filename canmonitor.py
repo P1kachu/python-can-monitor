c651462da2e58537f8d3939108dd2edd4ad6fee2
Stanislas P1kachu Lejay <p1kachu@lse.epita.fr>
2017-04-12 10:14:44 +0000

    Naive port to using socket-can (python3)
    Signed-off-by: Stanislas P1kachu Lejay <p1kachu@lse.epita.fr>

--- canmonitor.py
+++ canmonitor.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 import argparse
@@ -7,10 +7,10 @@ import sys
 import threading
 import traceback
 
-import serial
+import can
 
 should_redraw = threading.Event()
-stop_serial = threading.Event()
+stop_bus = threading.Event()
 
 can_messages = {}
 can_messages_lock = threading.Lock()
@@ -18,20 +18,29 @@ can_messages_lock = threading.Lock()
 thread_exception = None
 
 
-def read_until_newline(serial_device):
-    """Read data from `serial_device` until the next newline character."""
-    line = serial_device.readline()
-    while len(line) == 0 or line[-1] != '\n':
-        line = line + serial_device.readline()
+def read_bus(bus_device):
+    """Read data from `bus_device` until the next newline character."""
+    message = bus.recv(0.2)
+    while True:
+        if message:
+            break
+        message = bus.recv(0.2)
 
-    return line.strip()
+    try:
+        string = "{}:ID={}:LEN={}".format("RX", message.arbitration_id, message.dlc)
+        for x in range(message.dlc):
+            string += ":{:02x}".format(message.data[x])
+
+    except Exception as e:
+        print(e)
+    return string
 
 
-def serial_run_loop(serial_device):
+def bus_run_loop(bus_device):
     """Background thread for serial reading."""
     try:
-        while not stop_serial.is_set():
-            line = read_until_newline(serial_device)
+        while not stop_bus.is_set():
+            line = read_bus(bus_device)
 
             # Sample frame from Arduino: FRAME:ID=246:LEN=8:8E:62:1C:F6:1E:63:63:20
             # Split it into an array (e.g. ['FRAME', 'ID=246', 'LEN=8', '8E', '62', '1C', 'F6', '1E', '63', '63', '20'])
@@ -43,7 +52,8 @@ def serial_run_loop(serial_device):
 
                 data = [int(byte, 16) for byte in frame[3:]]  # convert the hex strings array to an integer array
                 data = [byte for byte in data if byte >= 0 and byte <= 255]  # sanity check
-
+    
+            
                 if len(data) != frame_length:
                     # Wrong frame length or invalid data
                     continue
@@ -52,11 +62,11 @@ def serial_run_loop(serial_device):
                 with can_messages_lock:
                     can_messages[frame_id] = data
                     should_redraw.set()
-            except:
+            except Exception as e:
                 # Invalid frame
                 continue
     except:
-        if not stop_serial.is_set():
+        if not stop_bus.is_set():
             # Only log exception if we were not going to stop the thread
             # When quitting, the main thread calls close() on the serial device
             # and read() may throw an exception. We don't want to display it as
@@ -78,7 +88,7 @@ def init_window(stdscr):
     return root_window
 
 
-def main(stdscr, serial_thread):
+def main(stdscr, bus_thread):
     """Main function displaying the UI."""
     # Don't print typed character
     curses.noecho()
@@ -102,7 +112,7 @@ def main(stdscr, serial_thread):
             # Compute row/column counts according to the window size and borders
             row_start = 3
             lines_per_column = max_y - (1 + row_start)
-            num_columns = (max_x - 2) / column_width
+            num_columns = int((max_x - 2) / column_width)
 
             # Setting up column headers
             for i in range(0, num_columns):
@@ -128,7 +138,7 @@ def main(stdscr, serial_thread):
                     # and spaces are replaced by '.'
                     msg_str = ''
                     for byte in msg:
-                        char = unichr(byte)
+                        char = chr(byte)
                         if char == '\0':
                             msg_str = msg_str + '.'
                         elif ord(char) < 32 or ord(char) > 126:
@@ -161,44 +171,39 @@ def main(stdscr, serial_thread):
             should_redraw.clear()
 
         c = stdscr.getch()
-        if c == ord('q') or not serial_thread.is_alive():
+        if c == ord('q') or not bus_thread.is_alive():
             break
         elif c == curses.KEY_RESIZE:
             win = init_window(stdscr)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Process CAN data from a serial device.')
-    parser.add_argument('serial_device', type=str)
-    parser.add_argument('baud_rate', type=int, default=115200,
-                        help='Serial baud rate in bps (default: 115200)')
+    parser = argparse.ArgumentParser(description='Process CAN data from a socket-can device.')
+    parser.add_argument('interface', default='can0')
 
     args = parser.parse_args()
 
-    serial_device = None
-    serial_thread = None
+    bus_device = None
+    bus_thread = None
 
     try:
-        # Open serial device with non-blocking read() (timeout=0)
-        serial_device = serial.Serial(args.serial_device, args.baud_rate, timeout=0)
+        bus = can.interface.Bus(channel=args.interface, bustype='socketcan_native')
 
-        # Start the serial reading background thread
-        serial_thread = threading.Thread(target=serial_run_loop, args=(serial_device,))
-        serial_thread.start()
+        # Start the bus reading background thread
+        bus_thread = threading.Thread(target=bus_run_loop, args=(bus,))
+        bus_thread.start()
 
-        # Make sure to draw the UI the first time even if there is no serial data
+        # Make sure to draw the UI the first time even if there is no data
         should_redraw.set()
 
         # Start the main loop
-        curses.wrapper(main, serial_thread)
+        curses.wrapper(main, bus_thread)
+
     finally:
-        # Cleanly stop serial thread before exiting
-        if serial_thread:
-            stop_serial.set()
+        # Cleanly stop bus thread before exiting
+        if bus_thread:
+            stop_bus.set()
 
-            if serial_device:
-                serial_device.close()
-
-            serial_thread.join()
+            bus_thread.join()
 
             # If the thread returned an exception, print it
             if thread_exception:
